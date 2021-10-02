8f3e7dcc11129982f5e3e2239ed635a402bb80db
GarryG00 <GarryG00@users.noreply.github.com>
2021-10-02 12:50:32 -0500

    RBNode comment

--- RedBlackNode.java
+++ RedBlackNode.java
@@ -2,10 +2,18 @@ public class RedBlackNode {
    
     //Constructor
     //key should only contain integers
-    int key;  // The data in the node
+    int key;  // The data/key in the node
     RedBlackNode left;     // Left child
     RedBlackNode right;    // Right child
-    Boolean color; 
+    Boolean color;  // Node color: True -> Red, False -> Black
+
+
+    /*
+    RedBlackNode(int val) -> NA
+        val: an integer that will be stored in the node as the key
+    This procedure creates a RBnode with the input as the key value. Assume the input is an integer.
+    Left and right children are both init to null and defult color for a new node is red
+    */
     RedBlackNode( int val )
     {
         key = val;
