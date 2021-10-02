c28e61fd6b7e4d5add5ee7eb577cf823eed37a9a
GarryG00 <GarryG00@users.noreply.github.com>
2021-10-02 13:06:29 -0500

    node object fix up

--- RedBlackNode.java
+++ RedBlackNode.java
@@ -1,8 +1,10 @@
+//RedBlackNode class contains the basic parameters of a RBnode 
 public class RedBlackNode {
    
     //Constructor
     //key should only contain integers
     int key;  // The data/key in the node
+    RedBlackTree p;     // the parent node
     RedBlackNode left;     // Left child
     RedBlackNode right;    // Right child
     Boolean color;  // Node color: True -> Red, False -> Black
@@ -11,7 +13,8 @@ public class RedBlackNode {
     /*
     RedBlackNode(int val) -> NA
         val: an integer that will be stored in the node as the key
-    This procedure creates a RBnode with the input as the key value. Assume the input is an integer.
+
+    Creates a RBnode with the input as the key value. Assume the input is an integer.
     Left and right children are both init to null and defult color for a new node is red
     */
     RedBlackNode( int val )
